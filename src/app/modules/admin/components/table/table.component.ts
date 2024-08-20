a79490b17c93c00c5ddded3edd02648c5c0983bf
pedrocv66 <cuevasp931@gmail.com>
2024-08-20 10:58:16 -0300

    vinculacion de modal para tomar y editar productos

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -12,6 +12,9 @@ export class TableComponent {
   // Creamos colección local de productos -> la definimos como array
   coleccionProductos: Producto[] = [];
 
+  productoSeleccionado!: Producto; // ! <- toma valores bacios
+  
+  modalVisibleProducto: boolean = false;
   // Definimos formulario para los productos
   /**
    * Atributos alfanuméricos (string) se inicializan con comillas simples
@@ -28,7 +31,13 @@ export class TableComponent {
 
   constructor(public servicioCrud: CrudService) { }
 
-  ngOnInit(): void { }
+
+  ngOnInit(): void {
+    this.servicioCrud.obtenerProducto().subscribe(producto => {
+      this.coleccionProductos = producto;
+    })
+
+  }
 
   async agregarProducto() {
     if (this.producto.valid) {
@@ -51,4 +60,49 @@ export class TableComponent {
         })
     }
   }
+
+  mostrarBorrar(productoSeleccionado: Producto) {
+    this.modalVisibleProducto = true;
+    this.productoSeleccionado = productoSeleccionado;
+  }
+
+  borrarProducto() {
+    this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto)
+      .then(respuesta => {
+        alert("se a podido eliminar con exito");
+      })
+      .catch(error => { alert("ha ocurriod un error al eliminar ");
+      })
+  }
+  //editar producto
+  mostrarEditar(productoSeleccionado: Producto){
+    this.producto.setValue({
+      nombre:productoSeleccionado.nombre,
+      precio: productoSeleccionado.precio,
+      descripcion: productoSeleccionado.descripcion,
+      categoria: productoSeleccionado.categoria,
+      imagen: productoSeleccionado.imagen,
+      alt: productoSeleccionado.alt
+    })
+  }
+  //vuncula editar priducto
+  editarProducto(){
+    let datos: Producto={
+      idProducto: this.productoSeleccionado.idProducto,
+      nombre: this.producto.value.nombre!,
+      precio: this.producto.value.precio!,
+      descripcion: this.producto.value.descripcion!,
+      categoria: this.producto.value.categoria!,
+      imagen: this.producto.value.imagen!,
+      alt:this.producto.value.alt!,
+  }
+    this.servicioCrud.modificarProducto(this.productoSeleccionado.idProducto,datos)
+    .then(producto=>{
+      alert("se modifico con exito")
+      this.producto.reset(),
+    })
+    .catch(error =>{
+      alert("error")
+    })
+}
 }
--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-
+import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -33,6 +33,34 @@ export class CrudService {
   }
 
   // OBTENER productos
+  obtenerProducto(){
+    /*
+      snapshotChanges => toma captura del estado de los datos
+      pipe => tuberías que retornan un nuevo arreglo
+      map => "mapea" o recorre esa nueva información
+      a => resguarda la nueva información y la envía como un documento 
+    */
+    return this.productosCollection.snapshotChanges().pipe(map(action => action.map(a => a.payload.doc.data())))
+  }
+
   // EDITAR productos
+  modificarProducto(idProducto: string, nuevaData: Producto){
+
+    /*
+    
+    */
+    return this.database.collection('producto').doc(idProducto).update(nuevaData);
+  }
   // ELIMINAR productos
+  eliminarProducto(idProducto: string){
+    return new Promise((resolve, reject) =>{
+      try{
+        const respuesta = this.productosCollection.doc(idProducto).delete();
+        resolve (respuesta);
+      }
+      catch(error){
+        reject(error);
+      }
+    })
+  }
 }
